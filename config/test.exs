71782228e6da49ee2ad9a0c1bb714d47a6c50071
Qing Wu <wiserfirst@gmail.com>
2024-03-16 00:00:45 +1100

    fix warnings

--- config/test.exs
+++ config/test.exs
@@ -1,9 +1,11 @@
-use Mix.Config
+import Config
 
 config :formex, ecto_repos: [Formex.TestRepo]
 
 config :formex,
   validator: Formex.Validator.Simple,
   translate_error: &Formex.TestErrorHelpers.translate_error/1
+
+config :phoenix, :json_library, Jason
 
 config :logger, :console, level: :info
